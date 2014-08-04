048a1a17a29ec4536a1e49436f64cd122a1d403a
Romain Deveaud <romain.deveaud@gmail.com>
2014-08-04 18:27:32 +0100

    settings file

--- utils/settings.R
+++ utils/settings.R
@@ -18,17 +18,64 @@
 # This file contains utilities for loading the settings and parameters
 # used by programs related to crawling and forecasting the attendance
 # of Foursquare venues.
+#
+# It is intended to mirror the `Settings` class of the FoursquareAttendanceCrawler
+# project (see https://github.com/SmartSearch/Foursquare-Attendance-Crawler).
 
 
-library(jsonlite)
+suppressPackageStartupMessages(library(jsonlite,quietly=T,verbose=F))
+
+settingsPath <- "/users/staff/romain/workspace/FoursquareAttendanceCrawler/etc/settings.json"
 
 loadedJSON <- NULL
 
-loadJSON <- function(path) {
-  loadedJSON <<- fromJSON(path)
+loadJSON <- function() {
+  loadedJSON <<- fromJSON(settingsPath)
 }
 
 getFolder <- function() {
+# Returns the folder where all the attendance data has been downloaded.
+  if(is.null(loadedJSON))
+    loadJSON()
+
+  path <- loadedJSON$crawl_folder
+  if(substr(path,nchar(path),nchar(path)) != .Platform$file.sep)
+    path <- paste(path, .Platform$file.sep, sep = "")
+
+  return(path)
 }
 
-loadJSON("/users/staff/romain/workspace/FoursquareAttendanceCrawler/etc/settings.json")
+getCityCenterLat <- function (city) {
+  if(is.null(loadedJSON))
+    loadJSON()
+
+  return(loadedJSON$centers[[city]]$lat)
+}
+
+getCityCenterLng <- function(city) {
+  if(is.null(loadedJSON))
+    loadJSON()
+
+  return(loadedJSON$centers[[city]]$lng)
+}
+
+getCityTimezone <- function(city) {
+  if(is.null(loadedJSON))
+    loadJSON()
+
+  return(loadedJSON$timezones[[city]])
+}
+
+getCityCredentialsSecret <- function(city) {
+  if(is.null(loadedJSON))
+    loadJSON()
+
+  return(loadedJSON$foursquare_api_accounts[[city]]$client_secret)
+}
+
+getCityCredentialsId <- function(city) {
+  if(is.null(loadedJSON))
+    loadJSON()
+
+  return(loadedJSON$foursquare_api_accounts[[city]]$client_id)
+}
